7b686362b49c8ca11aec77629cf508ea64cc7d4a
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-11-03 22:24:38 +0100

    Updated QCSubmission properties

--- OnePaceDbAccess/OnePaceDbAccess/Databases/OnePace/Models/QCSubmission.cs
+++ OnePaceDbAccess/OnePaceDbAccess/Databases/OnePace/Models/QCSubmission.cs
@@ -1,5 +1,4 @@
-﻿using OnePaceDbAccess.Databases.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnePaceDbAccess.Databases.OnePace.Models
@@ -12,15 +11,13 @@ namespace OnePaceDbAccess.Databases.OnePace.Models
         public int Id { get; set; }
         [Column("episode_id")]
         public int EpisodeId { get; set; }
-        [Column("status")]
-        public QCStatus Status { get; set; }
         [Column("link")]
         public string Link { get; set; }
+        [Column("ftp_path")]
+        public string FTPPath { get; set; }
         [Column("created_date")]
         public long CreatedDate { get; set; }
         [Column("created_by")]
         public string CreatedBy { get; set; }
-        [Column("version")]
-        public string Version { get; set; }
     }
 }
