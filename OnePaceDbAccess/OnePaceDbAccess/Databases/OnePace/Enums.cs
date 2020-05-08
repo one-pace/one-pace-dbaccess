ad39af3e94777c4921a795921101dea52e7446e0
Simon Haga <1498414+sewil@users.noreply.github.com>
2020-05-08 06:15:51 +0200

    Add resolutions enum

---
+++ OnePaceDbAccess/OnePaceDbAccess/Databases/OnePace/Enums.cs
@@ -0,0 +1,7 @@
+﻿namespace OnePaceDbAccess.Databases.OnePace.Enums
+{
+    public static class Enums
+    {
+        public static readonly string[] RESOLUTIONS = new string[] {"480p", "720p", "1080p"};
+    }
+}
