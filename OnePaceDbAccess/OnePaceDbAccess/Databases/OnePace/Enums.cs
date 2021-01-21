a89eb915a07050e1b7eecb34fa6e0ffd882495b8
Simon Haga <1498414+sewil@users.noreply.github.com>
2021-01-21 18:47:59 +0100

    Replace Enums.RESOLUTION with Resolution enum

--- OnePaceDbAccess/OnePaceDbAccess/Databases/OnePace/Enums.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OnePaceDbAccess.Databases.OnePace.Enums
-{
-    public static class Enums
-    {
-        public static readonly string[] RESOLUTIONS = new string[] {"480p", "720p", "1080p"};
-    }
-}
---
+++ OnePaceDbAccess/OnePaceDbAccess/Databases/OnePace/Enums/Resolution.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace OnePaceDbAccess.Databases.OnePace.Enums
+{
+    public enum Resolution
+    {
+        [Description("480p")]
+        SD = 1,
+        [Description("720p")]
+        HD = 2,
+        [Description("1080p")]
+        FHD = 3
+    }
+}
